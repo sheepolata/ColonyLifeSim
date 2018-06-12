76051a33239b4e0df32854a41309d94305b6f06e
a.gaget.personnal.laptop <antoine.gaget@gmail.com>
2018-06-12 14:53:03 +0100

    Big GUI Change : add a section to monitor and interact with the simu

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ import utils.basic_colors as basic_colors
 import pygame
 import random
 import sys
+import time
+import numpy as np
 
 
 from screeninfo import get_monitors
 monitor = get_monitors()[0]
 
-# width, height = 860, 680
-width, height = int(monitor.width*0.75), int(monitor.height*0.75)
+# main_surface_width, main_surface_height = 860, 680
+screen_width, screen_height = int(monitor.width*0.75), int(monitor.height*0.75)
+
+main_surface_width, main_surface_height = int(screen_width*0.75), int(screen_height)
+
+info_surface_width, info_surface_height = int(screen_width*0.25), int(screen_height)
+
 
 import os
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((monitor.width/2)-(width/2),(monitor.height/2)-(height/2))
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((monitor.width/2)-(screen_width/2),(monitor.height/2)-(screen_height/2))
 
 
 from pygame.locals import *
@@ -31,17 +38,20 @@ pygame.init()
 def main():
     DISPLAY_DEBUG = False
 
-    env = Env.Environment(width, height)
+    env = Env.Environment(main_surface_width, main_surface_height)
 
     clock = pygame.time.Clock()
 
-    screen = pygame.display.set_mode((width, height))
+    window = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Colony Life Sim")
-    alpha_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+
+    screen = pygame.Surface((main_surface_width, main_surface_height))
+    alpha_surface = pygame.Surface((main_surface_width, main_surface_height), pygame.SRCALPHA)
+    info_surface = pygame.Surface((info_surface_width, info_surface_height), pygame.SRCALPHA)
     
     for i in range(10):
         o = entities.Obstacle(random.randint(30, 100), random.randint(30, 100), env)
-        o.setRandomPose(width, height)
+        o.setRandomPose(main_surface_width, main_surface_height)
         env.addObstacle(o)
     env.splitEnvironment()
     env.constructGraph(Env.areNeigbhoursSquare)
@@ -51,14 +61,14 @@ def main():
     l_entities = []
     for i in range(10):
         entity = entities.NPC(env, "entity"+str(i))
-        entity.setRandomPose(width, height)
+        entity.setRandomPose(main_surface_width, main_surface_height)
         entity.setIdleBehaviour()
         l_entities.append(entity)
 
     l_spawner = []
-    for i in range(10):
+    for i in range(2):
         spawnerFood = entities.Spawner(env, "spawner"+str(i), "foodspawner", random.randint(560, 640), random.random()*0.6 + 0.8)
-        spawnerFood.setRandomPose(width, height)
+        spawnerFood.setRandomPose(main_surface_width, main_surface_height)
         spawnerFood.setSpawnerBehaviour()
         for i in range(3):
             spawnerFood.spawn()
@@ -66,11 +76,41 @@ def main():
 
 
     run = True
+
+    #Buttons
+    color_rect_button = basic_colors.BLUE
+    rect_button = pygame.Rect((main_surface_width + 10, main_surface_height*0.94), (info_surface_width*0.25, info_surface_height*0.05))
+    
+    color_quit_button = basic_colors.RED
+    quit_button = pygame.Rect((main_surface_width + info_surface_width*0.65, main_surface_height*0.94), 
+                                (info_surface_width*0.25, info_surface_height*0.05))
+    
+
+
+    q_time = []
+
     while run:
-        clock.tick(60) #tick at 60fps
+        t1 = time.time()
+
+        # clock.tick(60) #tick at 60fps
 
         screen.fill(basic_colors.GREEN)
         alpha_surface.fill(basic_colors.EMPTY)
+        info_surface.fill(basic_colors.WHITE)
+
+        mp = pygame.mouse.get_pos()
+
+        if quit_button.collidepoint(mp):
+            color_quit_button = basic_colors.RED_2
+        else:
+            color_quit_button = basic_colors.RED
+
+        if rect_button.collidepoint(mp):
+            color_rect_button = basic_colors.BLUE_2
+        elif DISPLAY_DEBUG:
+            color_rect_button = basic_colors.BLUE_3
+        else:
+            color_rect_button = basic_colors.BLUE
 
         #Single event control
         for event in pygame.event.get():
@@ -82,7 +122,6 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 #LMB
                 if event.button == 1:
-                    mp = pygame.mouse.get_pos()
                     rect = env.getCurrentRect(mp)
                     if rect != None:
                         for e in l_entities:
@@ -96,12 +135,16 @@ def main():
                                 e.behaviour.computePath()
                             else:
                                 e.setGOTOBehaviour(mp)
+                    if quit_button.collidepoint(mp):
+                        run = False
+                        color_quit_button = basic_colors.RED_3
+                    if rect_button.collidepoint(mp):
+                        DISPLAY_DEBUG = not DISPLAY_DEBUG
                 #MMB
                 if event.button == 2:
                     pass
                 #RMB
                 if event.button == 3:
-                    mp = pygame.mouse.get_pos()
                     rect = env.collideOneObstacle_Point(mp)
                     if rect != None:
                         res = entities.Ressource(env, "food", random.randint(30, 70), False)
@@ -159,8 +202,39 @@ def main():
             sp.sprite.draw(screen)
         
 
+        #Info surface
+        t2 = time.time()
+        diff_t = t2 - t1
 
-        screen.blit(alpha_surface, (0, 0))
+        fps = round(1.0 / diff_t, 0)
+        q_time.append(round(fps))
+        if len(q_time) >= 200 : q_time = q_time[1:]
+
+        #info text
+        text = "1 frame : " +  str(round(diff_t, 3)) + "s / " + str(round(np.mean(q_time))) + " fps"
+        font = pygame.font.SysFont('Sans', int(info_surface_height*0.02))
+        displ_text = font.render(text, True, basic_colors.BLACK)
+        info_surface.blit(displ_text, (10, 10))
+
+        #buttons
+        font = pygame.font.SysFont('Sans', int(info_surface_height*0.021))
+        quit_text = font.render("Quit (Esc)", True, basic_colors.BLACK)
+
+        rect_text = font.render("Rect (d)", True, basic_colors.BLACK)
+
+        #Blit and Flip surfaces
+        window.blit(screen, (0, 0))
+        window.blit(alpha_surface, (0, 0))
+        window.blit(info_surface, (main_surface_width, 0))
+
+        pygame.draw.rect(window, color_quit_button, quit_button)
+        window.blit(quit_text, (quit_button.center[0] - (quit_text.get_width()/2), 
+            quit_button.center[1] - (quit_text.get_height()/2)))
+
+        pygame.draw.rect(window, color_rect_button, rect_button)
+        window.blit(rect_text, (rect_button.center[0] - (rect_text.get_width()/2), 
+            rect_button.center[1] - (rect_text.get_height()/2)))
+
         pygame.display.flip()
 
     for e in l_entities:
