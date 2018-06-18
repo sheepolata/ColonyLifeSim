0c96fd53cdf5454c33f527b1938c2f2fddaa1744
a.gaget.personnal.laptop <antoine.gaget@gmail.com>
2018-06-18 12:00:55 +0100

    update requirements.txt. Tried to parallelise neighbours computation : unconclusive

--- entities/computationthread.py
+++ entities/computationthread.py
@@ -1,6 +1,11 @@
 import threading
 import utils.pathfinding as pf
 import utils.utils as utils
+import time
+
+from joblib import Parallel, delayed
+import multiprocessing
+
 
 class NeighboursComputationThread(threading.Thread):
     def __init__(self, env):
@@ -35,20 +40,45 @@ class NeighboursComputationThread(threading.Thread):
 
     def run(self):
         while self.running:
+            # t=time.time()
             self.computeNeighbours()
+            # print(time.time() - t)
 
     def computeNeighbours(self):
-        for focus in self.env.npcs:
-            if not focus in self.neighbours.keys(): self.neighbours[focus] = []
-            for npc in self.env.npcs:
-                if npc == focus: continue
 
-                if not pf.checkStraightPath(self.env, focus.getPose(), npc.getPose(), 10, check_river=False) and utils.distance2p(focus.getPose(), npc.getPose()) <= focus.vision_radius:
-                    if not npc in self.neighbours[focus]:
-                        self.neighbours[focus].append(npc)
-                elif npc in self.neighbours[focus]:
-                    self.neighbours[focus].remove(npc)
-        
+        for focus in self.env.npcs:
+            inloop_op_NCT(focus, self)
+
+        #multiprocessing solution : not working
+        # pool = multiprocessing.Pool(multiprocessing.cpu_count())
+        # zip(*pool.map(inloop_op_NCT, [focus for focus in self.env.npcs], self))
+
+        #Joblib solution : not good in multithreading
+        # num_cores = multiprocessing.cpu_count()
+        # Parallel(n_jobs=num_cores)(delayed(inloop_op_NCT)(focus, self) for focus in self.env.npcs)
+
+            # if not focus in self.neighbours.keys(): self.neighbours[focus] = []
+            # for npc in self.env.npcs:
+            #     if npc == focus: continue
+
+            #     if not pf.checkStraightPath(self.env, focus.getPose(), npc.getPose(), 10, check_river=False) and utils.distance2p(focus.getPose(), npc.getPose()) <= focus.vision_radius:
+            #         if not npc in self.neighbours[focus]:
+            #             self.neighbours[focus].append(npc)
+            #     elif npc in self.neighbours[focus]:
+            #         self.neighbours[focus].remove(npc)
+     
+def inloop_op_NCT(focus, ct):
+    if not focus in ct.neighbours.keys(): ct.neighbours[focus] = []
+
+    for npc in ct.env.npcs:
+        if npc == focus: continue
+
+        if not pf.checkStraightPath(ct.env, focus.getPose(), npc.getPose(), 10, check_river=False) and utils.distance2p(focus.getPose(), npc.getPose()) <= focus.vision_radius:
+            if not npc in ct.neighbours[focus]:
+                ct.neighbours[focus].append(npc)
+        elif npc in ct.neighbours[focus]:
+            ct.neighbours[focus].remove(npc)
+
 class ClosestFoodComputationThread(threading.Thread):
     def __init__(self, env):
         super(ClosestFoodComputationThread, self).__init__()
@@ -82,14 +112,25 @@ class ClosestFoodComputationThread(threading.Thread):
 
     def computeClosestFood(self):
         for focus in self.env.npcs:
-            if not focus in self.closestFood.keys(): self.closestFood[focus] = []
-            for f in self.env.ressources["food"]:
-                if not pf.checkStraightPath(self.env, focus.getPose(), f.getPose(), 10, check_river=False) and utils.distance2p(focus.getPose(), f.getPose()) <= focus.vision_radius:
-                    if not f in self.closestFood[focus]:
-                        self.closestFood[focus].append(f)
-                elif f in self.closestFood[focus]:
-                    self.closestFood[focus].remove(f)
+            inloop_op_CFCT(focus, self)
+            # if not focus in self.closestFood.keys(): self.closestFood[focus] = []
+            # for f in self.env.ressources["food"]:
+            #     if not pf.checkStraightPath(self.env, focus.getPose(), f.getPose(), 10, check_river=False) and utils.distance2p(focus.getPose(), f.getPose()) <= focus.vision_radius:
+            #         if not f in self.closestFood[focus]:
+            #             self.closestFood[focus].append(f)
+            #     elif f in self.closestFood[focus]:
+            #         self.closestFood[focus].remove(f)
 
     def run(self):
         while self.running:
             self.computeClosestFood()
+
+def inloop_op_CFCT(focus, ct):
+    for f in ct.env.ressources["food"]:
+        if not focus in ct.closestFood.keys(): ct.closestFood[focus] = []
+
+        if not pf.checkStraightPath(ct.env, focus.getPose(), f.getPose(), 10, check_river=False) and utils.distance2p(focus.getPose(), f.getPose()) <= focus.vision_radius:
+            if not f in ct.closestFood[focus]:
+                ct.closestFood[focus].append(f)
+        elif f in ct.closestFood[focus]:
+            ct.closestFood[focus].remove(f)
